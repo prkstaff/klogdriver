70651c31a640dc51e91353e9895ea19c36b84c8e
Renato Prado <prkstaff@gmail.com>
2020-07-03 14:29:09 -0300

    printing logs fine

--- main.go
+++ main.go
@@ -23,20 +23,37 @@ func main() {
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		text := input.Text()
-		r := regexp.MustCompile(`(?P<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z)\s(?P<regularexpression>{.*}$)`)
+		r := regexp.MustCompile(`(?P<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).\d+Z\s(?P<regularexpression>{.*}$)`)
 		match := r.FindStringSubmatch(text)
-		timestamp := match[1]
-		jsonString := match[2]
-		var jsonObject map[string]interface{}
-		err := json.Unmarshal([]byte(jsonString), &jsonObject)
-		if err != nil {
-			fmt.Println(text)
+		if len(match) > 2 {
+			timestamp := match[1]
+			jsonString := match[2]
+			var jsonObject map[string]interface{}
+			err := json.Unmarshal([]byte(jsonString), &jsonObject)
+			if err != nil {
+				fmt.Println(text)
+			} else {
+				cpf := jsonObject["cpf"]
+				module := jsonObject["module"]
+				if cpf == nil {
+					cpf = "None"
+				}
+				logLine := fmt.Sprintf("%s", jsonObject["message"])
+				//print timestamp without newline
+				fmt.Printf("\033[1;90m%s [%s] [%s] \033[0m", timestamp, cpf, module)
+
+				if jsonObject["severity"] == "INFO" {
+					color.White(logLine)
+				} else if jsonObject["severity"] == "ERROR" {
+					color.Red(logLine)
+				} else {
+					color.Yellow(logLine)
+				}
+			}
 		} else {
-			fmt.Println(timestamp)
-			fmt.Println(jsonObject)
+			fmt.Println(text)
 		}
 	}
-
 	color.Cyan("Cyan")
 	color.Blue("Blue")
 }
